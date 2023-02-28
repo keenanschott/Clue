0f1ddf65949df9a4691aee5e1b3edadff915df3c
keenanschott <keenanmschott28@gmail.com>
2023-02-27 22:04:38 -0700

    gitignore changes

--- src/tests/BoardTestsExp.java
+++ src/tests/BoardTestsExp.java
@@ -1,6 +1,10 @@
 package tests;
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 import java.util.Set;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import experiment.TestBoard;
 import experiment.TestBoardCell;
@@ -17,12 +21,20 @@ public class BoardTestsExp {
     public void testAdjacency() {   
         TestBoardCell cell = board.getCell(0,0);
         Set<TestBoardCell> testList = cell.getAdjList();
-        Assert.assertTrue();
+        Assert.assertTrue(testList.contains(board.getCell(1, 0)));
+        Assert.assertTrue(testList.contains(board.getCell(0, 1)));
+        Assert.assertTrue(testList.contains(board.getCell(0, 0)));
+        Assert.assertTrue(testList.contains(board.getCell(3, 3)));
+        Assert.assertTrue(testList.contains(board.getCell(2, 3)));
+        Assert.assertTrue(testList.contains(board.getCell(3, 0)));
     }
 
     @Test 
-    public void testTargetCreation() {
-
-    }
+    public void testTargetsNormal() {
+    	TestBoardCell cell = board.getCell(0,0);
+        board.calcTargets(cell, 3);
+        Set<TestBoardCell> targets = board.getTargets();
+        Assert.assertEquals();
+        }
 
 }
