b154c044ab960c58a4d77e11075f6bff5d118b6c
keenanschott <keenanmschott28@gmail.com>
2023-02-28 10:14:09 -0700

    Adjacencies calculated

--- src/experiment/TestBoard.java
+++ src/experiment/TestBoard.java
@@ -9,12 +9,42 @@ public class TestBoard {
 	final static int ROWS = 4;
 
     public TestBoard() { // empty constructor as prescribed
-    	grid = new TestBoardCell[4][4]; // for testing
+    	grid = new TestBoardCell[ROWS][COLS]; // for testing
+    	for (int i = 0; i < ROWS; i++) {
+    		for (int j = 0; j < COLS; j++) {
+    			TestBoardCell testCell = new TestBoardCell(i, j);
+    			grid[i][j] = testCell; // fill grid with cells
+    		}
+    	}
+    	for (int i = 0; i < ROWS; i++) {
+    		for (int j = 0; j < COLS; j++) {
+    			if (i != 0) {
+    				grid[i][j].addAdjacency(grid[i - 1][j]);
+    			}
+    			if (i != 3) {
+    				grid[i][j].addAdjacency(grid[i + 1][j]);
+    			}
+    			if (j != 0) {
+    				grid[i][j].addAdjacency(grid[i][j - 1]);
+    			}
+    			if (j != 3) {
+    				grid[i][j].addAdjacency(grid[i][j + 1]);
+    			}
+    		}
+    	}
     	targets = new HashSet<TestBoardCell>();
     	visited = new HashSet<TestBoardCell>();
     }
 
     public void calcTargets(TestBoardCell startCell, int pathLength) { // function stub
+    	visited.add(startCell);
+    	findAllTargets();	
+    }
+    
+    private void 
+    
+    private void findAllTargets(TestBoardCell startCell, int pathLength) {
+    	
     }
 
     public TestBoardCell getCell(int row, int col) {
