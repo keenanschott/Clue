a2ab1132ad8cc39c8b3170bb3d9098f7e9ef324d
Keenan Schott <keenanmschott28@gmail.com>
2023-03-26 21:56:07 -0600

    reorganized function order in Board

--- src/clueGame/Board.java
+++ src/clueGame/Board.java
@@ -49,6 +49,148 @@ public class Board {
     }
 
 	/**
+     * Connect instance variables to requested input files.
+	 * 
+	 * @param layoutCSV The requested layout CSV file.
+	 * @param setupTXT The requested setup text file. 
+     */
+	public void setConfigFiles(String layoutCSV, String setupTXT) {
+		layoutConfigFile = "data/" + layoutCSV; // our files are stored in a data folder
+		setupConfigFile = "data/" + setupTXT;
+	}
+
+	/**
+     * Load the setup file and check for validity.
+	 * 
+	 * @throws BadConfigFormatException
+     */
+    public void loadSetupConfig() throws BadConfigFormatException {
+		roomMap = new HashMap<Character,Room>(); // allocate space for our map
+        File file = new File(setupConfigFile); // file object
+		String currentLine; // current line
+		String[] lineArray; // array of words in current line
+		try {
+			Scanner scan = new Scanner(file); // open
+			while (scan.hasNext()) {
+				currentLine = scan.nextLine(); // get entire line
+				lineArray = currentLine.split(", "); // split by comma
+				if (lineArray.length == 3 && (lineArray[0].equals("Room") || lineArray[0].equals("Space"))) { // valid line
+					Room newRoom = new Room(lineArray[1], null, null); // do not insert center or label yet
+					roomMap.put(lineArray[2].charAt(0), newRoom); // put into map
+				}
+				else {
+					if (!lineArray[0].substring(0, 2).equals("//")) { // if not a comment
+						scan.close(); // close before exception
+						throw new BadConfigFormatException();
+					}
+				}
+			}
+			scan.close(); // close
+		} catch (FileNotFoundException e) {
+			e.printStackTrace(); 
+		}
+    }
+
+	/**
+     * Load the layout file and check for validity.
+	 * 
+	 * @throws BadConfigFormatException
+     */
+    public void loadLayoutConfig() throws BadConfigFormatException {
+        ArrayList<String[]> allLinesLayout = new ArrayList<String[]>(); // new ArrayList of Strings
+		File file = new File(layoutConfigFile); // file object
+		String currentLine; // current line
+		String[] lineArray; // array of words in current line
+		try {
+			Scanner sc = new Scanner(file); // open
+			while (sc.hasNext()) {
+				currentLine = sc.nextLine();
+				lineArray = currentLine.split(",");
+				allLinesLayout.add(lineArray);
+			}
+			numRows = allLinesLayout.size(); // number of rows is equal to size of allLinesLayout
+			numColumns = allLinesLayout.get(0).length; // number of columns is equal to size of any entry in allLinesLayout
+			sc.close(); // close
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		// fill the grid given allLinesLayout
+		fillGrid(allLinesLayout);
+    }
+
+		/**
+     * Fill the grid given a two-dimensional array of Strings.
+	 * 
+	 * @param layout Contains all cells as strings. 
+	 * @throws BadConfigFormatException
+     */
+	private void fillGrid(ArrayList<String[]> layout) throws BadConfigFormatException {
+		grid = new BoardCell[numRows][numColumns]; 
+		int rowCounter = 0; // count rows
+		int colCounter = 0; // count columns
+		for (String[] row : layout) {
+			for (String cell : row) { // for a given cell String
+				if (cell.length() < 1 || cell.length() > 2) {
+					throw new BadConfigFormatException("Layout file contains configuration errors!"); // invalid length of String; invalid cell
+				} else {
+					BoardCell newCell = new BoardCell(rowCounter, colCounter, cell.charAt(0)); // new cell
+					if (roomMap.containsKey(cell.charAt(0))) { // a valid cell with regards to our map
+						fillGridCellHelper(newCell); // set cell booleans depending on the initial; see fillGridCellHelper
+						if(cell.length() == 2) { // if a cell with special behavior
+							fillGridRoomHelper(cell, newCell); // see fillGridRoomHelper
+						}				
+					}
+					else {
+						throw new BadConfigFormatException("Layout file specifies a room that is not in the legend!"); // not in setup file
+					}
+					grid[rowCounter][colCounter] = newCell; // fill grid with newCell
+					colCounter++; // next column, same row
+				}
+			}
+			rowCounter++; // iterate to the next row
+			if (colCounter != numColumns) {
+				throw new BadConfigFormatException("Layout file that does not have the same number of columns for each row!"); // invalid row; did not have a matching column count
+			}
+			colCounter = 0; // reset column count to start from left for the next row
+		}
+	}
+
+	/**
+     * Set cell booleans depending on the initial.
+	 * 
+	 * @param cell The cell under examination.
+     */
+	private void fillGridCellHelper(BoardCell cell) {
+		if (cell.getInitial() != 'W' && cell.getInitial() != 'X') {
+			cell.setIsRoom(true); // if not W or X, the cell is a room
+		}
+		else if (cell.getInitial() == 'X') {
+			cell.setOccupied(true); // if X, set the cell to be occupied
+		}	
+	}
+
+	/**
+     * Change aspects of the Room and BoardCell depending on the special characters observed.
+	 * 
+	 * @param cellString The string that represents the given cell.
+	 * @param cell The corresponding BoardCell.
+     */
+	private void fillGridRoomHelper(String cellString, BoardCell cell) {
+		Room changeRoom = roomMap.get(cellString.charAt(0)); // temporary Room object to change the room within the map
+		if(cellString.charAt(1) == '#') { // label cell
+			cell.setLabel(true);
+			changeRoom.setLabelCell(cell);
+		} else if (cellString.charAt(1) == '*') { // center cell
+			cell.setCenter(true);
+			changeRoom.setCenterCell(cell);
+		} else if (cellString.charAt(0) == 'W') { // doorway cell
+			cell.setDoorDirection(cellString.charAt(1));
+		} else {
+			cell.setSecretPassage(cellString.charAt(1)); // only remaining special cell option
+		}
+	}
+
+	/**
      * Create an adjacency list for every cell within the game board.
 	 * 
 	 * @param grid The game board; create adjacency lists for every cell within it.
@@ -125,148 +267,6 @@ public class Board {
 	}
 
 	/**
-     * Connect instance variables to requested input files.
-	 * 
-	 * @param layoutCSV The requested layout CSV file.
-	 * @param setupTXT The requested setup text file. 
-     */
-	public void setConfigFiles(String layoutCSV, String setupTXT) {
-		layoutConfigFile = "data/" + layoutCSV; // our files are stored in a data folder
-		setupConfigFile = "data/" + setupTXT;
-	}
-
-	/**
-     * Load the setup file and check for validity.
-	 * 
-	 * @throws BadConfigFormatException
-     */
-    public void loadSetupConfig() throws BadConfigFormatException {
-		roomMap = new HashMap<Character,Room>(); // allocate space for our map
-        File file = new File(setupConfigFile); // file object
-		String currentLine; // current line
-		String[] lineArray; // array of words in current line
-		try {
-			Scanner scan = new Scanner(file); // open
-			while (scan.hasNext()) {
-				currentLine = scan.nextLine(); // get entire line
-				lineArray = currentLine.split(", "); // split by comma
-				if (lineArray.length == 3 && (lineArray[0].equals("Room") || lineArray[0].equals("Space"))) { // valid line
-					Room newRoom = new Room(lineArray[1], null, null); // do not insert center or label yet
-					roomMap.put(lineArray[2].charAt(0), newRoom); // put into map
-				}
-				else {
-					if (!lineArray[0].substring(0, 2).equals("//")) { // if not a comment
-						scan.close(); // close before exception
-						throw new BadConfigFormatException();
-					}
-				}
-			}
-			scan.close(); // close
-		} catch (FileNotFoundException e) {
-			e.printStackTrace(); 
-		}
-    }
-
-	/**
-     * Load the layout file and check for validity.
-	 * 
-	 * @throws BadConfigFormatException
-     */
-    public void loadLayoutConfig() throws BadConfigFormatException {
-        ArrayList<String[]> allLinesLayout = new ArrayList<String[]>(); // new ArrayList of Strings
-		File file = new File(layoutConfigFile); // file object
-		String currentLine; // current line
-		String[] lineArray; // array of words in current line
-		try {
-			Scanner sc = new Scanner(file); // open
-			while (sc.hasNext()) {
-				currentLine = sc.nextLine();
-				lineArray = currentLine.split(",");
-				allLinesLayout.add(lineArray);
-			}
-			numRows = allLinesLayout.size(); // number of rows is equal to size of allLinesLayout
-			numColumns = allLinesLayout.get(0).length; // number of columns is equal to size of any entry in allLinesLayout
-			sc.close(); // close
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		}
-		// fill the grid given allLinesLayout
-		fillGrid(allLinesLayout);
-    }
-
-	/**
-     * Fill the grid given a two-dimensional array of Strings.
-	 * 
-	 * @param layout Contains all cells as strings. 
-	 * @throws BadConfigFormatException
-     */
-	private void fillGrid(ArrayList<String[]> layout) throws BadConfigFormatException {
-		grid = new BoardCell[numRows][numColumns]; 
-		int rowCounter = 0; // count rows
-		int colCounter = 0; // count columns
-		for (String[] row : layout) {
-			for (String cell : row) { // for a given cell String
-				if (cell.length() < 1 || cell.length() > 2) {
-					throw new BadConfigFormatException("Layout file contains configuration errors!"); // invalid length of String; invalid cell
-				} else {
-					BoardCell newCell = new BoardCell(rowCounter, colCounter, cell.charAt(0)); // new cell
-					if (roomMap.containsKey(cell.charAt(0))) { // a valid cell with regards to our map
-						fillGridCellHelper(newCell); // set cell booleans depending on the initial; see fillGridCellHelper
-						if(cell.length() == 2) { // if a cell with special behavior
-							fillGridRoomHelper(cell, newCell); // see fillGridRoomHelper
-						}				
-					}
-					else {
-						throw new BadConfigFormatException("Layout file specifies a room that is not in the legend!"); // not in setup file
-					}
-					grid[rowCounter][colCounter] = newCell; // fill grid with newCell
-					colCounter++; // next column, same row
-				}
-			}
-			rowCounter++; // iterate to the next row
-			if (colCounter != numColumns) {
-				throw new BadConfigFormatException("Layout file that does not have the same number of columns for each row!"); // invalid row; did not have a matching column count
-			}
-			colCounter = 0; // reset column count to start from left for the next row
-		}
-	}
-
-	/**
-     * Set cell booleans depending on the initial.
-	 * 
-	 * @param cell The cell under examination.
-     */
-	private void fillGridCellHelper(BoardCell cell) {
-		if (cell.getInitial() != 'W' && cell.getInitial() != 'X') {
-			cell.setIsRoom(true); // if not W or X, the cell is a room
-		}
-		else if (cell.getInitial() == 'X') {
-			cell.setOccupied(true); // if X, set the cell to be occupied
-		}	
-	}
-
-	/**
-     * Change aspects of the Room and BoardCell depending on the special characters observed.
-	 * 
-	 * @param cellString The string that represents the given cell.
-	 * @param cell The corresponding BoardCell.
-     */
-	private void fillGridRoomHelper(String cellString, BoardCell cell) {
-		Room changeRoom = roomMap.get(cellString.charAt(0)); // temporary Room object to change the room within the map
-		if(cellString.charAt(1) == '#') { // label cell
-			cell.setLabel(true);
-			changeRoom.setLabelCell(cell);
-		} else if (cellString.charAt(1) == '*') { // center cell
-			cell.setCenter(true);
-			changeRoom.setCenterCell(cell);
-		} else if (cellString.charAt(0) == 'W') { // doorway cell
-			cell.setDoorDirection(cellString.charAt(1));
-		} else {
-			cell.setSecretPassage(cellString.charAt(1)); // only remaining special cell option
-		}
-	}
-
-	/**
      * Calculate all valid targets to move to.
 	 * 
 	 * @param startCell The starting cell to examine as it pertains to the pathLength.
@@ -330,13 +330,6 @@ public class Board {
 		return targets; // return target cells
 	}
 
-	/**
-     * Get a cell from the grid.
-	 * 
-	 * @param row The requested row.
-	 * @param col The requested column.
-	 * @return Returns the BoardCell from that location in the grid.
-     */
 	public BoardCell getCell(int row, int col) {
 		return grid[row][col]; // returns the cell at the given parameters
 	}
