2b7bac260303cd5e26863fec9fe9ef96b35fe9fa
Keenan Schott <keenanmschott28@gmail.com>
2023-03-31 21:35:50 -0600

    comments in Card and Solution

--- src/clueGame/Card.java
+++ src/clueGame/Card.java
@@ -1,27 +1,48 @@
 package clueGame;
 
+/**
+ * Card
+ * A singular card; has a name and a type which constitute the hands of all of the players.
+ * DATE: 3/31/2023
+ * @author Keenan Schott
+ * @author Finn Burns
+ */
 public class Card {
-    private String cardName;
-    private CardType type;
+    private String cardName; // the name associated with the card
+    private CardType type; // the card type
 
+    /**
+	 * Card()
+     * Constructor.
+     * 
+     * @param name The requested card name.
+     * @param cardType The requested card type.
+     */
     public Card(String name, CardType cardType) {
         super();
         cardName = name;
         type = cardType;
     }
 
+    /**
+	 * equals()
+     * Compare two Card instances.
+	 * 
+	 * @param o The comparison object.
+     */
     @Override
     public boolean equals(Object o) {
         if (o == this) {
-            return true;
+            return true; // if it's itself; true
         }
         if (!(o instanceof Card)) {
-            return false;
+            return false; // if not a Card instance; false
         }
-        Card c = (Card) o;
-        return (c.getType() == getType() && c.getName().equals(getName()));
+        Card c = (Card) o; // cast
+        return (c.getType() == getType() && c.getName().equals(getName())); // compare instance variables
     }
 
+    // all getters and setters
     public CardType getType() {
         return type;
     }
--- src/clueGame/Solution.java
+++ src/clueGame/Solution.java
@@ -1,14 +1,26 @@
 package clueGame;
 
+/**
+ * Solution
+ * The solution to the game; has one weapon card, one room card, and one person card. The human player attempts to deduce this solution to win the game.
+ * DATE: 3/31/2023
+ * @author Keenan Schott
+ * @author Finn Burns
+ */
 public class Solution {
     private Card room;
     private Card person;
     private Card weapon;
 
+    /**
+	 * Solution()
+     * Constructor.
+     */
     public Solution() {
         super();
     }
 
+    // all getters and setters
     public void setRoom(Card inRoom) {
         room = inRoom;
     }
@@ -32,5 +44,4 @@ public class Solution {
     public Card getWeapon() {
         return weapon;
     }
-
 }
