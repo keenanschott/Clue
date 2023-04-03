7e49f1d12927c0881834e831050cf26c11306532
finn <90734575+squilliamb@users.noreply.github.com>
2023-04-02 19:52:05 -0600

    initial workings of ComputerAITest and GameSolutionTest

--- src/tests/GameSolutionTest.java
+++ src/tests/GameSolutionTest.java
@@ -1,5 +1,28 @@
 package tests;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import clueGame.*;
+
 public class GameSolutionTest {
+    private static Board board;
+    
+    @BeforeAll
+	public static void setUp() {
+		// Board is singleton, get the only instances
+		board = Board.getInstance();
+		// set the file names to use my config files
+		board.setConfigFiles("ClueLayout.csv", "ClueSetup.txt");
+		// Initialize will load config files 
+		board.initialize();
+        board.deal();
+	}
     
 }
