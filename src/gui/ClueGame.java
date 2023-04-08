954fb9c4bedba3c3ac4e5cf55f4690afb3652152
Keenan Schott <keenanmschott28@gmail.com>
2023-04-08 11:28:31 -0600

    need help

--- src/gui/ClueGame.java
+++ src/gui/ClueGame.java
@@ -8,16 +8,16 @@ import java.awt.BorderLayout;
 import java.awt.GridLayout;
 
 public class ClueGame extends JFrame {
-    private CardsPanel rightPanel;
-    private GameControlPanel bottomPanel;
+    private CardsPanel rightPanel = new CardsPanel();
+    private GameControlPanel bottomPanel = new GameControlPanel();
 
     public ClueGame() {
         super();
-        rightPanel = new CardsPanel();
-        bottomPanel = new GameControlPanel();
-        setLayout(new GridLayout(2, 2));
+
+        setLayout(new GridLayout(0, 1));
         add(rightPanel, BorderLayout.EAST);
-        add(bottomPanel, BorderLayout.SOUTH);
+        rightPanel.setVisible(true);
+        //add(bottomPanel, BorderLayout.SOUTH);
         setContentPane(rightPanel);
         setSize(1000, 1000);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
