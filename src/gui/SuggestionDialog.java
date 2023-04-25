e8b7648d72042c3bea24f3ee608e73dee00f023a
keenanschott <keenanmschott28@gmail.com>
2023-04-25 11:04:12 -0600

    Comments done; refactoring starting point

--- src/gui/SuggestionDialog.java
+++ src/gui/SuggestionDialog.java
@@ -19,6 +19,14 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
+/**
+ * SuggestionDialog
+ * The game's dialog for a human player's suggestion.
+ * DATE: 4/25/2023
+ * 
+ * @author Keenan Schott
+ * @author Finn Burns
+ */
 public class SuggestionDialog extends JDialog {
     JLabel leftOne;
     JLabel leftTwo;
@@ -45,19 +53,18 @@ public class SuggestionDialog extends JDialog {
         rightFour.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                dispose();
+                dispose(); // "Cancel" button functionality
             }
         });
-
         leftFour.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 Solution suggestion = new Solution(rightOne.getText(), (String)rightTwo.getSelectedItem(), (String)rightThree.getSelectedItem());
                 if (!suggestion.getPerson().equals(new Card(Board.getInstance().getCurrentPlayer().getName(), CardType.PERSON))) {
-                    Board.getInstance().moveSuggestedPlayer(Board.getInstance().getCurrentPlayer(), suggestion);
+                    Board.getInstance().moveSuggestedPlayer(Board.getInstance().getCurrentPlayer(), suggestion); // move suggested player
                 }
                 Card evidence = Board.getInstance().handleSuggestion(Board.getInstance().getCurrentPlayer(), suggestion);
-                // guess is concatenation of person name, room, weapon and we pass in color of that player
+                // guess is concatenation of person name, room, and weapon, and we pass in color of that player
                 ClueGame.getBottomPanel().setGuess(suggestion.getPerson().getName() + ", " + suggestion.getRoom().getName() + ", " + suggestion.getWeapon().getName(), Board.getInstance().getCurrentPlayer().getColor());
                 if (evidence == null) {
                     ClueGame.getBottomPanel().setGuessResultColor(null);
@@ -72,7 +79,7 @@ public class SuggestionDialog extends JDialog {
                     ClueGame.getRightPanel().addSeen(evidence, CardsPanel.getBgColor());
                 }
                 Board.getInstance().getCurrentPlayer().getSeenCards().add(evidence);
-                dispose();
+                dispose(); // close window
             }
         });
         add(leftOne, BorderLayout.WEST);
@@ -93,9 +100,12 @@ public class SuggestionDialog extends JDialog {
      * @param args The list of arguments.
      */
     public static void main(String[] args) {
+        Board testBoard = Board.getInstance(); // initialize board
+        testBoard.setConfigFiles("ClueLayout.csv", "ClueSetup.txt");
+        testBoard.initialize();
+        testBoard.deal();
         SuggestionDialog test = new SuggestionDialog(new Room("Kitchen", null, null));
         test.setSize(500, 500);
         test.setVisible(true);
-    }
-    
+    }   
 }
